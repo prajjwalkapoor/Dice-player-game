920a5a37992ccf68049a242e36bc8445cfdc3434
prajjwalkapoor <68912239+prajjwalkapoor@users.noreply.github.com>
2021-03-15 00:40:09 +0530

    second commit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-
-// for player 1
+//Function to roll the dice
+function rollTheDice() {
+    // for player 1
 var randomNumber1 = Math.floor(((Math.random())*6)+1)
 var imgSrc = "images/dice" + randomNumber1 + ".png"
 document.querySelectorAll("img")[0].setAttribute("src",imgSrc)
@@ -11,8 +12,7 @@ document.querySelectorAll("img")[1].setAttribute("src",imgSrc)
 
 
 //Who won message]
-var player1 = "Player 1"
-var player2 = "Player 2"
+
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = player1+ "- Wins!🚩 ";
   }
@@ -22,17 +22,13 @@ if (randomNumber1 > randomNumber2) {
   else {
     document.querySelector("h1").innerHTML = "Draw!";
   }
+}
 
-// button handelers
-// Changing players name
-document.querySelectorAll(".btn")[1].addEventListener("click", changeName)
-function changeName() {
-     player1 = prompt("enter player1 name")
-     player2 = prompt("enter player2 name")     
-        document.querySelectorAll("h2")[0].innerHTML=player1;
-        document.querySelectorAll("h2")[1].innerHTML=player2;
-        document.querySelector("h1").innerHTML = player1+ "- Wins!🚩 ";
-        document.querySelector("h1").innerHTML = player2+ "- Wins!🚩 ";       
-} 
+function nameEdit() {
 
+    player1 = prompt("enter player1 name")
+    player2 = prompt("enter player2 name")     
+       document.querySelectorAll("h2")[0].innerHTML=player1;
+       document.querySelectorAll("h2")[1].innerHTML=player2;
 
+}
